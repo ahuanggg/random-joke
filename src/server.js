a667374d03a3143f24312f928793b32b83f12003
ahuanggg <ah6595@rit.edu>
2021-02-22 22:32:30 -0500

    un commented some code

--- src/server.js
+++ src/server.js
@@ -5,32 +5,32 @@ const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const responseHandler = require('./responses.js');
 
-//const port = process.env.PORT || process.env.NODE_PORT || 3000;
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-	'/random-joke': responseHandler.getRandomJokeResponse,
-	'/style.css': htmlHandler.getCSS,
-	notFound: htmlHandler.getError,
+  '/random-joke': responseHandler.getRandomJokeResponse,
+  '/style.css': htmlHandler.getCSS,
+  notFound: htmlHandler.getError,
 };
 
 const onRequest = (request, response) => {
-	const parsedUrl = url.parse(request.url);
-	const { pathname } = parsedUrl;
+  const parsedUrl = url.parse(request.url);
+  const { pathname } = parsedUrl;
 
-	const httpMethod = request.method;
+  const httpMethod = request.method;
 
-	let acceptedTypes = request.headers.accept && request.headers.accept.split(',');
-	acceptedTypes = acceptedTypes || [];
+  let acceptedTypes = request.headers.accept && request.headers.accept.split(',');
+  acceptedTypes = acceptedTypes || [];
 
-	const params = query.parse(parsedUrl.query);
+  const params = query.parse(parsedUrl.query);
 
-	if (pathname === '/random-joke') {
-		urlStruct[pathname](request, response, params, acceptedTypes, httpMethod);
-	} else if ('/style.css') {
-		urlStruct[pathname](request, response);
-	} else {
-		urlStruct.notFound(request, response);
-	}
+  if (pathname === '/random-joke') {
+    urlStruct[pathname](request, response, params, acceptedTypes, httpMethod);
+  } else if ('/style.css') {
+    urlStruct[pathname](request, response);
+  } else {
+    urlStruct.notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port);
